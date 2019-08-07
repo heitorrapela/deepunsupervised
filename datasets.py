7630e94e09f70657a049721f16e7498bdf352d7f
Pedro Magalhaes <pedromagalhaes.hb@gmail.com>
2019-08-06 23:45:53 -0300

    Handling Datasets

--- datasets.py
+++ datasets.py
@@ -5,9 +5,34 @@ import pandas as pd
 
 from scipy.io import arff
 from torch.utils.data import Dataset
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch.utils.data.dataloader import DataLoader
+from os.path import join
 
 
-class ArffDataset(Dataset):
+class Datasets(Dataset):
+
+    def __init__(self, dataset, root_folder="Datasets/", batch_size=32):
+
+        transform = transforms.Compose([transforms.ToTensor()])
+
+        if dataset == "mnist":
+            data = datasets.MNIST(root=root_folder, train=True, download=True, transform=transform)
+            test_data = datasets.MNIST(root=root_folder, train=False, download=True, transform=transform)
+
+        elif dataset == "fashion":
+            data = datasets.FashionMNIST(root=root_folder, train=True, download=True, transform=transform)
+            test_data = datasets.FashionMNIST(root=root_folder, train=False, download=True, transform=transform)
+        else:
+            data = CustomDataset(load_path=join(root_folder, dataset))
+            test_data = data
+
+        self.train_loader = DataLoader(data, batch_size=batch_size, shuffle=True)
+        self.test_loader = DataLoader(test_data, shuffle=False)
+
+
+class CustomDataset(Dataset):
 
     def __init__(self, load_path):
 
@@ -21,7 +46,7 @@ class ArffDataset(Dataset):
         self.y = data.iloc[:, -1].values
 
         self.X = data.iloc[:, :-1]
-        
+
         if load_path != "":
             self.y = self.y.astype(int)
 
--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from Models.som import SOM
-from datasets import ArffDataset
+from datasets import Datasets
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torchvision.datasets as datasets
-import torchvision.transforms as transforms
-from torch.utils.data.dataloader import DataLoader
 from sklearn.metrics.cluster import adjusted_rand_score
 from sklearn.metrics.cluster import completeness_score
 from sklearn.metrics.cluster import homogeneity_score
@@ -16,19 +13,12 @@ import os
 import argparse
 import numpy as np
 
-def read_lines(file_path):
-    if os.path.isfile(file_path):
-        data = open(file_path, 'r')
-        data = np.array(data.read().splitlines())
-    else:
-        data = []
 
-    return data
-
-def argumentParser():
+def argument_parser():
 	# Set args
 	parser = argparse.ArgumentParser(description='Self Organizing Map')
 	parser.add_argument('--dataset', type=str, default='mnist', help='Dataset Name')
+	parser.add_argument('--root', type=str, default='Datasets', help='Dataset Root folder')
 	parser.add_argument('--batch_size', type=int, default=32, help='input batch size')
 	parser.add_argument('--epochs', type=int, default=3, help='input total epoch')
 	parser.add_argument('--row', type=int, default=10, help='set SOM row length')
@@ -40,67 +30,40 @@ def argumentParser():
 if __name__ == '__main__':
 	
 	# Argument Parser
-	args = argumentParser()	
+	args = argument_parser()
 	dataset = args.dataset
+	root = args.root
 	batch_size = args.batch_size
 	epochs = args.epochs 
 	device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 	use_cuda = True
 
-	transform = transforms.Compose(
-		[transforms.ToTensor()]
-	)
-	
 	train_data = None
 	train_loader = None
 	test_data = None
 	test_loader = None
 
-	if(dataset == "mnist"):
-		train_data = datasets.MNIST(root='Datasets/',train=True, download=True, transform=transform)
-		train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
-		test_data = datasets.MNIST(root='Datasets/',train=False, download=True, transform=transform)
-		test_loader = DataLoader(test_data, batch_size=1, shuffle=False)
-		som_input_height = 28
-		som_input_width = 28
-		som_input_deep = 1
+	data = Datasets(dataset=dataset, root_folder=root)
 
-	elif(dataset == "fashion"):
-		train_data = datasets.FashionMNIST(root='Datasets/',train=True, download=True, transform=transform)
-		train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
-		test_data = datasets.FashionMNIST(root='Datasets/',train=False, download=True, transform=transform)
-		test_loader = DataLoader(test_data, batch_size=1, shuffle=False)
-		som_input_height = 28
-		som_input_width = 28
-		som_input_deep = 1
-	elif(dataset == "custom"):
-		## Custom data
-		train = 'Datasets/Realdata/breast.arff'
-		train_data = ArffDataset(train)
-		train_loader = DataLoader(train_data,batch_size=batch_size,shuffle=True)
-
-		test_data = train_data
-		test_loader = DataLoader(test_data, batch_size=1, shuffle=False)
-
-		som_input_height = 33
-		som_input_width = 1
-		som_input_deep = 1
+	som_input_height = 28
+	som_input_width = 28
+	som_input_deep = 1
 
 	# Varia de acordo com o dataset ou features, deixei assim por enquanto :)
 	som_input_dim = som_input_height*som_input_width*som_input_deep
 
-	som = SOM(input_size=som_input_dim, out_size=(args.row,args.col),use_cuda=use_cuda)
+	som = SOM(input_size=som_input_dim, out_size=(args.row, args.col), use_cuda=use_cuda)
 	som = som.to(device)
 
 	for epoch in range(epochs):
-		for batch_idx, (data, target) in enumerate(train_loader):
-			data, target = data.to(device), target.to(device)
-			som_loss, _ = som.forward(data) # Se quiser adicionar a loss com outra
-			som.self_organizing(data.view(-1, som_input_dim), epoch, epochs) # Faz forward e ajuste
+		for batch_idx, (sample, target) in enumerate(data.train_loader):
+			sample, target = sample.to(device), target.to(device)
+			som_loss, _ = som.forward(sample)  # Se quiser adicionar a loss com outra
+			som.self_organizing(sample.view(-1, som_input_dim), epoch, epochs)  # Faz forward e ajuste
 			if batch_idx % args.loginterval == 0:
 				print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss SOM: {:.6f}'.format(
-				epoch, batch_idx * len(data), len(train_loader.dataset),
-				100. * batch_idx / len(train_loader), som_loss))
+					epoch, batch_idx * len(sample), len(data.train_loader.dataset),
+					100. * batch_idx / len(data.train_loader), som_loss))
 
 	## Need to change train loader to test loader...
 	cluster_result, predict_labels,true_labels = som.cluster(test_loader)
