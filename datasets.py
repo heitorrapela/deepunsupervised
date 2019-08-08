16a29ff951893906c78383a989d5d086072242fa
Pedro Magalhaes <pedromagalhaes.hb@gmail.com>
2019-08-08 20:04:14 -0300

    Handling Datasets [3]

--- datasets.py
+++ datasets.py
@@ -5,79 +5,97 @@ import pandas as pd
 
 from scipy.io import arff
 from sklearn import preprocessing
-from torch.utils.data import Dataset
+import torch.utils.data as data
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
-from torch.utils.data.dataloader import DataLoader
 from os.path import join
 
 
-class Datasets(Dataset):
+class Datasets(data.Dataset):
 
-    def __init__(self, dataset, root_folder="Datasets/", batch_size=32):
+    def __init__(self, dataset, root_folder="Datasets/"):
+        super(Datasets, self).__init__()
 
-        transform = transforms.Compose([transforms.ToTensor()])
+        transform = transforms.Compose([transforms.ToTensor(),
+                                        ReshapeTransform((-1,))])
 
         if dataset == "mnist":
-            data = datasets.MNIST(root=root_folder, train=True, download=True, transform=transform)
-            test_data = datasets.MNIST(root=root_folder, train=False, download=True, transform=transform)
+            self.train_data = datasets.MNIST(root=root_folder, train=True, download=True, transform=transform)
+            self.test_data = datasets.MNIST(root=root_folder, train=False, download=True, transform=transform)
+            self.dim_flatten = self.train_data.data.size(1) * self.train_data.data.size(2)
 
         elif dataset == "fashion":
-            data = datasets.FashionMNIST(root=root_folder, train=True, download=True, transform=transform)
-            test_data = datasets.FashionMNIST(root=root_folder, train=False, download=True, transform=transform)
+            self.train_data = datasets.FashionMNIST(root=root_folder, train=True, download=True, transform=transform)
+            self.test_data = datasets.FashionMNIST(root=root_folder, train=False, download=True, transform=transform)
+            self.dim_flatten = self.train_data.data.size(1) * self.train_data.data.size(2)
 
-        elif dataset == "Ccifar10":
-            data = datasets.CIFAR10(root=root_folder, train=True, download=True, transform=transform)
-            test_data = datasets.CIFAR10(root=root_folder, train=False, download=True, transform=transform)
+        elif dataset == "cifar10":
+            self.train_data = datasets.CIFAR10(root=root_folder, train=True, download=True, transform=transform)
+            self.test_data = datasets.CIFAR10(root=root_folder, train=False, download=True, transform=transform)
+            data_shape = self.train_data.data.shape
+            self.dim_flatten = data_shape[1] * data_shape[2] * data_shape[3]
 
         elif dataset == "cifar100":
-            data = datasets.CIFAR100(root=root_folder, train=True, download=True, transform=transform)
-            test_data = datasets.CIFAR100(root=root_folder, train=False, download=True, transform=transform)
+            self.train_data = datasets.CIFAR100(root=root_folder, train=True, download=True, transform=transform)
+            self.test_data = datasets.CIFAR100(root=root_folder, train=False, download=True, transform=transform)
+            data_shape = self.train_data.data.shape
+            self.dim_flatten = data_shape[1] * data_shape[2] * data_shape[3]
 
         elif dataset == "svhn":
-            data = datasets.SVHN(root=root_folder, train=True, download=True, transform=transform)
-            test_data = datasets.SVHN(root=root_folder, train=False, download=True, transform=transform)
+            self.train_data = datasets.SVHN(root=root_folder, split='train', download=True, transform=transform)
+            self.test_data = datasets.SVHN(root=root_folder, split='test', download=True, transform=transform)
+            data_shape = self.train_data.data.shape
+            self.dim_flatten = data_shape[1] * data_shape[2] * data_shape[3]
+
         else:
-            data = CustomDataset(load_path=join(root_folder, dataset), norm="minmax")
-            test_data = data
-
-        self.train_loader = DataLoader(data, batch_size=batch_size, shuffle=True)
-        self.test_loader = DataLoader(test_data, shuffle=False)
+            self.train_data = CustomDataset(load_path=join(root_folder, dataset), norm="minmax")
+            self.test_data = self.train_data
+            self.dim_flatten = self.train_data.data.shape[1]
 
 
-class CustomDataset(Dataset):
+class CustomDataset(data.Dataset):
 
     def __init__(self, load_path, norm=None):
+        super(CustomDataset, self).__init__()
 
         if load_path.endswith(".arff"):
-            data, meta = arff.loadarff(load_path)
-            data = pd.DataFrame(data, dtype=float)
+            loaded_mat, meta = arff.loadarff(load_path)
+            loaded_mat = pd.DataFrame(loaded_mat, dtype=float)
         else:
-            data = pd.read_csv(load_path, sep=",", header=None)
-            data = pd.DataFrame(data, dtype=float)
+            loaded_mat = pd.read_csv(load_path, sep=",", header=None)
+            loaded_mat = pd.DataFrame(loaded_mat, dtype=float)
 
         if norm is not None:
             if norm == 'minmax':
-                min_max_scaler = preprocessing.MinMaxScaler().fit(data)
-                data = min_max_scaler.transform(data)
+                min_max_scaler = preprocessing.MinMaxScaler().fit(loaded_mat)
+                loaded_mat = min_max_scaler.transform(loaded_mat)
 
             elif norm == 'scaler':
-                scaler = preprocessing.StandardScaler().fit(data)
-                data = scaler.transform(data)
+                scaler = preprocessing.StandardScaler().fit(loaded_mat)
+                loaded_mat = scaler.transform(loaded_mat)
 
-        data = pd.DataFrame(data, dtype=float)
+        loaded_mat = pd.DataFrame(loaded_mat, dtype=float)
 
-        self.y = data.iloc[:, -1].values
+        self.labels = loaded_mat.iloc[:, -1].values
 
-        self.X = data.iloc[:, :-1]
+        self.data = loaded_mat.iloc[:, :-1]
 
         if load_path != "":
-            self.y = self.y.astype(int)
+            self.labels = self.labels.astype(int)
 
     def __getitem__(self, index):
-        data = torch.tensor(self.X.iloc[index])
-        target = torch.tensor(self.y[index], dtype=torch.long)
-        return data, target
+        sample, target = self.data.iloc[index], int(self.labels[index])
+
+        return torch.tensor(sample), torch.tensor(target)
 
     def __len__(self):
-        return self.X.shape[0]
+        return len(self.data)
+
+
+class ReshapeTransform(object):
+
+    def __init__(self, size):
+        self.size = size
+
+    def __call__(self, sample):
+        return torch.reshape(sample, self.size)
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from sklearn.metrics.cluster import adjusted_rand_score
+from torch.utils.data.dataloader import DataLoader
 from sklearn.metrics.cluster import completeness_score
 from sklearn.metrics.cluster import homogeneity_score
 from sklearn.metrics.cluster import v_measure_score
@@ -17,8 +18,8 @@ import numpy as np
 def argument_parser():
 	# Set args
 	parser = argparse.ArgumentParser(description='Self Organizing Map')
-	parser.add_argument('--dataset', type=str, default='breast.arff', help='Dataset Name')
-	parser.add_argument('--root', type=str, default='Datasets/Realdata', help='Dataset Root folder')
+	parser.add_argument('--dataset', type=str, default='mnist', help='Dataset Name')
+	parser.add_argument('--root', type=str, default='Datasets/', help='Dataset Root folder')
 	parser.add_argument('--batch-size', type=int, default=1, help='input batch size')
 	parser.add_argument('--epochs', type=int, default=3, help='input total epoch')
 	parser.add_argument('--loginterval', type=int, default=1, help='Log Interval')
@@ -36,29 +37,25 @@ if __name__ == '__main__':
 	device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 	use_cuda = True
 
-	data = Datasets(dataset=dataset, root_folder=root)
+	dataset = Datasets(dataset=dataset, root_folder=root)
 
-	som_input_height = 33
-	som_input_width = 1
-	som_input_deep = 1
+	train_loader = DataLoader(dataset.train_data, batch_size=batch_size, shuffle=True)
+	test_loader = DataLoader(dataset.test_data, shuffle=False)
 
-	# Varia de acordo com o dataset ou features, deixei assim por enquanto :)
-	som_input_dim = som_input_height*som_input_width*som_input_deep
-
-	som = SOM(input_size=som_input_dim)
+	som = SOM(input_size=dataset.dim_flatten)
 	som = som.to(device)
 
 	for epoch in range(epochs):
-		for batch_idx, (sample, target) in enumerate(data.train_loader):
+		for batch_idx, (sample, target) in enumerate(train_loader):
 			sample, target = sample.to(device), target.to(device)
 			som_loss, _ = som.self_organizing(sample, epoch, epochs)  # Faz forward e ajuste
 			if batch_idx % args.loginterval == 0:
 				print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss SOM: {:.6f}'.format(
-					epoch, batch_idx * len(sample), len(data.train_loader.dataset),
-					100. * batch_idx / len(data.train_loader), som_loss))
+					epoch, batch_idx * len(sample), len(train_loader.dataset),
+					100. * batch_idx / len(train_loader), som_loss))
 
 	## Need to change train loader to test loader...
-	cluster_result, predict_labels, true_labels = som.cluster(data.test_loader)
+	cluster_result, predict_labels, true_labels = som.cluster(test_loader)
 	som.write_output(dataset + ".results", cluster_result)
 	#print(np.asarray(predict_labels).shape,np.asarray(true_labels).shape)
 	#print(adjusted_rand_score(true_labels,predict_labels))
