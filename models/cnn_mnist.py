83a199840e9e83d07f50e23ffafa548317297f65
heitorrapela <hrm@cin.ufpe.br>
2019-11-13 15:49:35 -0300

    Add dynamic CNN version

--- models/cnn_mnist.py
+++ models/cnn_mnist.py
@@ -7,49 +7,54 @@ from models.som import SOM
 
 
 class Net(nn.Module):
-    def __init__(self, d_in=1, hw_in=28, som_input=2, filters_list=[20, 50], kernel_size_list=[5, 5],
+    def __init__(self, d_in=1, n_conv_layers=3, max_pool=True,hw_in=28, som_input=2, filters_list=[20, 50], kernel_size_list=[5, 5],
                  stride_size_list=[1, 1], padding_size_list=[0, 0], max_pool2d_size=2, device='cpu'):
         super(Net, self).__init__(),
 
         self.som_input_size = som_input
         self.d_in = d_in
-        self.hw_in = hw_in
-        self.filters_list = filters_list
+        self.hw_out = hw_in
+        self.max_pool = max_pool
+        self.n_conv_layers = n_conv_layers
+        self.filters_list = [d_in] + filters_list
         self.max_pool2d_size = max_pool2d_size
         self.kernel_size_list = kernel_size_list
         self.padding_size_list = padding_size_list
         self.stride_size_list = stride_size_list
+        self.convs = []
 
-        self.conv1 = nn.Conv2d( self.d_in,
-                                self.filters_list[0],
-                                self.kernel_size_list[0],
-                                self.stride_size_list[0],
-                                self.padding_size_list[0])
-
-
-        self.hw_out = ((self.hw_in + 2*self.padding_size_list[0]-(self.kernel_size_list[0]-1)-1)//self.stride_size_list[0])+1
-        self.hw_out = self.hw_out//self.max_pool2d_size
-
-        self.conv2 = nn.Conv2d( self.filters_list[0],
-                                self.filters_list[1],
-                                self.kernel_size_list[1],
-                                self.stride_size_list[1],
-                                self.padding_size_list[1])
-
-        self.hw_out = ((self.hw_out + 2*self.padding_size_list[1]-(self.kernel_size_list[1]-1)-1)//self.stride_size_list[1])+1
-        self.hw_out = self.hw_out//self.max_pool2d_size
-
-        self.fc1 = nn.Linear(self.hw_out * self.hw_out * self.filters_list[-1], self.som_input_size)
+        last_hw_out = self.hw_out 
+        for i in range(n_conv_layers):
+            if(not (i < len(self.padding_size_list) and i < len(self.kernel_size_list) and i < len(self.stride_size_list))):
+                print("Warning the size of the padding, kernel or stride list is too small!")
+                break
+            last_hw_out = ((self.hw_out + 2*self.padding_size_list[i]-(self.kernel_size_list[i]-1)-1)//self.stride_size_list[i])+1
+            if(last_hw_out >= 2):
+                self.hw_out = last_hw_out
+                self.hw_out  = self.hw_out//self.max_pool2d_size if self.max_pool else self.hw_out
+                self.convs.append(nn.Sequential(nn.Conv2d( self.filters_list[i],
+                                                self.filters_list[i+1],
+                                                self.kernel_size_list[i],
+                                                self.stride_size_list[i],
+                                                self.padding_size_list[i]),
+                                                nn.ReLU(),
+                                                nn.MaxPool2d(self.max_pool2d_size, self.max_pool2d_size) if self.max_pool else nn.Identity()
+                                                )
+                                )
+            else:
+                print("Warning the size of the output is too small!")
+                break
+            # print(self.hw_out)
+        
+        self.fc1 = nn.Linear(self.hw_out*self.hw_out*self.filters_list[len(self.filters_list) - len(self.convs) - 2], self.som_input_size)
         self.device = device
         self.som = SOM(input_dim=self.som_input_size, device=self.device)
         self.som = self.som.to(self.device)
 
     def cnn_extract_features(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x, self.max_pool2d_size, self.max_pool2d_size)
-        x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x, self.max_pool2d_size, self.max_pool2d_size)
-        x = x.view(-1, self.hw_out*self.hw_out*self.filters_list[-1])
+        for i in range(len(self.convs)):
+            x = self.convs[i](x)
+        x = x.view(-1, self.hw_out*self.hw_out*self.filters_list[len(self.filters_list) - len(self.convs) - 2])
         x = self.fc1(x)
         x = torch.tanh(x)
         return x
