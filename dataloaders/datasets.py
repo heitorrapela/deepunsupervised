a1e1dd618f8f1472bf786abbb220623cd52026e0
heitorrapela <hrm@cin.ufpe.br>
2019-11-18 19:54:15 -0300

    Add cifar10 and cifar100 data augm and preproc same as L2C algorithm

--- dataloaders/datasets.py
+++ dataloaders/datasets.py
@@ -16,18 +16,21 @@ class Datasets(data.Dataset):
     def __init__(self, dataset, root_folder="raw-datasets/", flatten=False, debug=False, n_samples=100):
         super(Datasets, self).__init__()
 
-        transform_list = [transforms.ToTensor()]
+        transform_train_list = []
+        transform_test_list = []
 
         if dataset == "mnist":
-            transform_list.append(transforms.Normalize((0.1307,), (0.3081,)))
 
+            transform_train_list.append(transforms.ToTensor())
+            transform_train_list.append(transforms.Normalize((0.1307,), (0.3081,)))
             if flatten:
-                transform_list.append(ReshapeTransform((-1,)))
+                transform_train_list.append(ReshapeTransform((-1,)))
 
-            transform = transforms.Compose(transform_list)
+            transform_train = transforms.Compose(transform_train_list)
+            transform_test = transform_train
 
-            self.train_data = datasets.MNIST(root=root_folder, train=True, download=True, transform=transform)
-            self.test_data = datasets.MNIST(root=root_folder, train=False, download=True, transform=transform)
+            self.train_data = datasets.MNIST(root=root_folder, train=True, download=True, transform=transform_train)
+            self.test_data = datasets.MNIST(root=root_folder, train=False, download=True, transform=transform_test)
             
             if debug:
                 self.train_data.data = self.train_data.data[:n_samples]
@@ -39,15 +42,18 @@ class Datasets(data.Dataset):
             self.hw_in = 28
 
         elif dataset == "fashion":
-            transform_list.append(transforms.Normalize((0.5,), (0.5,)))
+
+            transform_train_list.append(transforms.ToTensor())
+            transform_train_list.append(transforms.Normalize((0.5,), (0.5,)))
 
             if flatten:
-                transform_list.append(ReshapeTransform((-1,)))
+                transform_train_list.append(ReshapeTransform((-1,)))
 
-            transform = transforms.Compose(transform_list)
+            transform_train = transforms.Compose(transform_train_list)
+            transform_test = transform_train
 
-            self.train_data = datasets.FashionMNIST(root=root_folder, train=True, download=True, transform=transform)
-            self.test_data = datasets.FashionMNIST(root=root_folder, train=False, download=True, transform=transform)
+            self.train_data = datasets.FashionMNIST(root=root_folder, train=True, download=True, transform=transform_train)
+            self.test_data = datasets.FashionMNIST(root=root_folder, train=False, download=True, transform=transform_test)
 
             if debug:
                 self.train_data.data = self.train_data.data[:n_samples]
@@ -60,13 +66,23 @@ class Datasets(data.Dataset):
 
         elif dataset == "cifar10":
 
+            transform_train_list.append(transforms.RandomCrop(32, padding=4))
+            transform_train_list.append(transforms.RandomHorizontalFlip())
+            transform_train_list.append(transforms.ToTensor())
+            transform_train_list.append(transforms.Normalize(mean=[0.491, 0.482, 0.447], std=[0.247, 0.243, 0.262]))
+
+            transform_test_list.append(transforms.ToTensor())
+            transform_test_list.append(transforms.Normalize(mean=[0.491, 0.482, 0.447], std=[0.247, 0.243, 0.262]))
+
             if flatten:
-                transform_list.append(ReshapeTransform((-1,)))
+                transform_train_list.append(ReshapeTransform((-1,)))
+                transform_test_list.append(ReshapeTransform((-1,)))
 
-            transform = transforms.Compose(transform_list)
+            transform_train = transforms.Compose(transform_train_list)
+            transform_test = transforms.Compose(transform_test_list)
 
-            self.train_data = datasets.CIFAR10(root=root_folder, train=True, download=True, transform=transform)
-            self.test_data = datasets.CIFAR10(root=root_folder, train=False, download=True, transform=transform)
+            self.train_data = datasets.CIFAR10(root=root_folder, train=True, download=True, transform=transform_train)
+            self.test_data = datasets.CIFAR10(root=root_folder, train=False, download=True, transform=transform_test)
             
             if debug:
                 self.train_data.data = self.train_data.data[:n_samples]
@@ -79,13 +95,24 @@ class Datasets(data.Dataset):
 
         elif dataset == "cifar100":
 
+            transform_train_list.append(transforms.RandomCrop(32, padding=4))
+            transform_train_list.append(transforms.RandomHorizontalFlip())
+            transform_train_list.append(transforms.ToTensor())
+            transform_train_list.append(transforms.Normalize(mean=[0.507, 0.487, 0.441], std=[0.267, 0.256, 0.276]))
+
+            transform_test_list.append(transforms.ToTensor())
+            transform_test_list.append(transforms.Normalize(mean=[0.507, 0.487, 0.441], std=[0.267, 0.256, 0.276]))
+
             if flatten:
-                transform_list.append(ReshapeTransform((-1,)))
+                transform_train_list.append(ReshapeTransform((-1,)))
+                transform_test_list.append(ReshapeTransform((-1,)))
 
-            transform = transforms.Compose(transform_list)
+            transform_train = transforms.Compose(transform_train_list)
+            transform_test = transforms.Compose(transform_test_list)
 
-            self.train_data = datasets.CIFAR100(root=root_folder, train=True, download=True, transform=transform)
-            self.test_data = datasets.CIFAR100(root=root_folder, train=False, download=True, transform=transform)
+            self.train_data = datasets.CIFAR100(root=root_folder, train=True, download=True, transform=transform_train)
+            self.test_data = datasets.CIFAR100(root=root_folder, train=False, download=True, transform=transform_test)
+
 
             if debug:
                 self.train_data.data = self.train_data.data[:n_samples]
@@ -98,13 +125,15 @@ class Datasets(data.Dataset):
 
         elif dataset == "svhn":
 
+            transform_train_list.append(transforms.ToTensor())
             if flatten:
-                transform_list.append(ReshapeTransform((-1,)))
+                transform_train_list.append(ReshapeTransform((-1,)))
 
-            transform = transforms.Compose(transform_list)
+            transform_train = transforms.Compose(transform_train_list)
+            transform_test = transform_train
 
-            self.train_data = datasets.SVHN(root=root_folder, split='train', download=True, transform=transform)
-            self.test_data = datasets.SVHN(root=root_folder, split='test', download=True, transform=transform)
+            self.train_data = datasets.SVHN(root=root_folder, split='train', download=True, transform=transform_train)
+            self.test_data = datasets.SVHN(root=root_folder, split='test', download=True, transform=transform_test)
 
             if debug:
                 self.train_data.data = self.train_data.data[:n_samples]
@@ -117,13 +146,15 @@ class Datasets(data.Dataset):
 
         elif dataset == "usps":
 
+            transform_train_list.append(transforms.ToTensor())
             if flatten:
-                transform_list.append(ReshapeTransform((-1,)))
+                transform_train_list.append(ReshapeTransform((-1,)))
 
-            transform = transforms.Compose(transform_list)
+            transform_train = transforms.Compose(transform_train_list)
+            transform_test = transform_train
 
-            self.train_data = datasets.USPS(root=root_folder, train=True, download=True, transform=transform)
-            self.test_data = datasets.USPS(root=root_folder, train=False, download=True, transform=transform)
+            self.train_data = datasets.USPS(root=root_folder, train=True, download=True, transform=transform_train)
+            self.test_data = datasets.USPS(root=root_folder, train=False, download=True, transform=transform_test)
 
             if debug:
                 self.train_data.data = self.train_data.data[:n_samples]
@@ -133,7 +164,7 @@ class Datasets(data.Dataset):
             self.dim_flatten = data_shape[1] * data_shape[2]
             self.d_in = 1
             self.hw_in = 16
-            
+
         else:
             self.train_data = CustomDataset(load_path=join(root_folder, dataset), norm="minmax")
             self.test_data = self.train_data
